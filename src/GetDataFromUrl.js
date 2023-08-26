62e0b08cc7a5ce59ed6eca2570c14a4366cba592
Sarkhan <abdullayevsarxan92@gmail.com>
2023-08-26 17:21:26 +0400

    refactoring: change the delete logic

--- src/GetDataFromUrl.js
+++ src/GetDataFromUrl.js
@@ -8,6 +8,7 @@ const GetDataFromUrl = ({ url, fieldsToShow, modalFieldsToShow }) => {
     const [cardRow, setCardRow] = useState(3);
     const [selectedItem, setSelectedItem] = useState(null);
     const [editingItem, setEditingItem] = useState(null);
+    const [deletingItem, setDeletingItem] = useState(null);
     const data = Store.getState().data;
     const [adding, setAdding] = useState(false);
     const [newItem, setNewItem] = useState({
@@ -63,23 +64,22 @@ const GetDataFromUrl = ({ url, fieldsToShow, modalFieldsToShow }) => {
         setEditingItem(null);
     };
 
-    const deleteClick = (item) => {
-        setEditingItem(item);
-    };
-
-    const deleteItem = () => {
-        const updatedData = data.filter((item) => item.id !== editingItem.id);
-        Store.dispatch(setData(updatedData));
-
-        setEditingItem(null);
-    };
-
     const changeColor = (item) => {
         const colors = ['bg-primary', 'bg-danger', 'bg-success'];
         const randomColorClass =
             colors[Math.floor(Math.random() * colors.length)];
         item.colorClass = randomColorClass;
         setCardRow(cardRow + 1);
+    };
+
+    const deleteClick = (item) => {
+        setDeletingItem(item);
+    };
+
+    const deleteItem = () => {
+        const updatedData = data.filter((item) => item.id !== deletingItem.id);
+        Store.dispatch(setData(updatedData));
+        setDeletingItem(null);
     };
 
     return (
@@ -128,6 +128,7 @@ const GetDataFromUrl = ({ url, fieldsToShow, modalFieldsToShow }) => {
                     </button>
                 </div>
             )}
+
             {loading ? (
                 <p>Loading...</p>
             ) : (
@@ -222,13 +223,28 @@ const GetDataFromUrl = ({ url, fieldsToShow, modalFieldsToShow }) => {
                         >
                             Update
                         </button>
-
-                        <button className="btn btn-danger" onClick={deleteItem}>
-                            Delete
-                        </button>
                     </div>
                 )}
             </Modal>
+
+            {deletingItem && (
+                <Modal
+                    title="Удаление записи"
+                    isOpen={deletingItem !== null}
+                    onClose={() => setDeletingItem(null)}
+                >
+                    <p>Do you really want to delete this card?</p>
+                    <button className="btn btn-danger" onClick={deleteItem}>
+                        Yes
+                    </button>
+                    <button
+                        className="btn btn-secondary"
+                        onClick={() => setDeletingItem(null)}
+                    >
+                        No
+                    </button>
+                </Modal>
+            )}
         </div>
     );
 };
